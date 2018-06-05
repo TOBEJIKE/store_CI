15726ea543c85a2dc5773935e79c416b4045817a
TOBEJIKE <3066902228@qq.com>
2018-06-05 23:59:04 +0800

    0605

--- application/services/User_service.php
+++ application/services/User_service.php
@@ -1,14 +1,10 @@
 <?php
 class User_service extends MY_Service
 {
-    // 配置路径
-    // const CONFIG_PATH = 'service/user';
-
     function __construct()
     {
         parent::__construct();
         $this->load->model('user_model');
-        // $this->config->load(self::CONFIG_PATH, TRUE);
     }
     // 测试数据
     public function user_list(){
